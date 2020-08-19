5e0cc5cc52c163685292aac1941b869884870772
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-19 14:32:11 +0000

    adds 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby_store = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: 1, womens_apparel: 1)
+richmond_store = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: 0, womens_apparel: 1)
+gastown_store = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: 1, womens_apparel: 0)
+
+puts Store.count
+
