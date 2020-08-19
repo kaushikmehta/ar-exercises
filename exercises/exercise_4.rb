2362caf61b0d77e2c59b75cea9d3702c0820f21c
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-19 15:04:16 +0000

    gets stores based on conditions

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey_store = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: 0, womens_apparel: 1)
+whistler_store = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: 1, womens_apparel: 0)
+yaletown_store = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: 1, womens_apparel: 1)
+
+@mens_stores = Store.where(mens_apparel: 1)
+
+@mens_stores.each do |mens_store|
+  puts "#{mens_store.name} does $#{mens_store.annual_revenue} in sales every year"
+end
+
+@womens_stores = Store.where(womens_apparel: 1).where("annual_revenue < ?", 1000000)
+
+@womens_stores.each do |womens_store|
+  puts "#{womens_store.name} does $#{womens_store.annual_revenue} in sales every year"
+end
