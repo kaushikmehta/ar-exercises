d4e1c62497bef7885e5703a21c9c6778561d7b4b
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-19 14:38:15 +0000

    creates, modifies and deletes using ActiveRecord

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,5 +8,5 @@ burnaby_store = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_appar
 richmond_store = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: 0, womens_apparel: 1)
 gastown_store = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: 1, womens_apparel: 0)
 
-puts Store.count
 
+puts Store.count
